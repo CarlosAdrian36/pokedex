dfbd3a63e56b95d6e357ede92fec28684961fb14
Carlos.Arroyo <carlos23_adrian@hotmail.com>
2025-09-29 15:22:04 -0600

    ConfigService

--- src/main.ts
+++ src/main.ts
@@ -18,5 +18,6 @@ async function bootstrap() {
   );
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`App runnin on ${process.env.PORT}`);
 }
 bootstrap();
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -11,13 +11,19 @@ import { isValidObjectId, Model } from "mongoose";
 import { Pokemon } from "./entities/pokemon.entity";
 import { InjectModel } from "@nestjs/mongoose";
 import { PaginationDto } from "../common/dto/pagination.dto";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class PokemonService {
+  private defaultLimit: number | undefined;
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ) {}
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly configService: ConfigService
+  ) {
+    this.defaultLimit = configService.get("defaultlimit");
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -30,7 +36,7 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit!, offset = 0 } = paginationDto;
 
     return this.pokemonModel
       .find()
